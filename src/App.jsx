e8122f4426b0213c6ddf6e85ffca2bdf9f5ad429
MD SIFAT AHAMED~ <mdsifat978@gmail.com>
2024-12-17 12:00:59 +0600

    main

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
 import React from 'react';
 import Header from './Components/Header/Header';
 import Shop from './Components/Shop/Shop';
-
+import { BrowserRouter, Routes, Route } from "react-router";
+import Review from './Components/Review/Review';
+import Inventory from './Components/Inventory/Inventory';
+import NotFound from './Components/NotFound/NotFound';
 const App = () => {
   return (
     <div>
       <Header/>
-      <Shop/>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/shop" element={<Shop />} />
+          <Route path="/review" element={<Review/>} />
+          <Route path="/inventory" element={<Inventory/>} />
+          <Route exact path="/" element={<Shop/>}/>
+          <Route path="*" element={<NotFound/>}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 };
