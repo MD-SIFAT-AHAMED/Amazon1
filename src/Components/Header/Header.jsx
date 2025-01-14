4b9abeda7ba3a65d9c0cdd0ae57c435063ce29b0
MD SIFAT AHAMED~ <mdsifat978@gmail.com>
2025-01-14 23:59:32 +0600

    main

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,10 +1,15 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import img from "././../../images/logo.png";
 import "./Header.css";
 import { Link } from 'react-router';
+import { UserContext } from '../../App';
 const Header = () => {
+  const [loggedInUser,setLoggedInUser]= useContext(UserContext);
     return (
+      
         <div className='HeaderContainer'>
+          <h2>name: {loggedInUser.name}</h2>
+          <h2>email: {loggedInUser.email}</h2>
            <div className='imgContainer'>
              <img className='imgHeader' src={img} alt="" />
            </div>
@@ -12,6 +17,7 @@ const Header = () => {
                 <Link to="/shop">Shop</Link>
                 <Link to="/review">Review</Link>
                 <Link to="/inventory">Manage Inventory</Link>
+                <button onClick={()=>setLoggedInUser({})}>Sign Out</button>
            </nav>
         </div>
     );
